2aaf9e5f44aedeb3101cf9e4e5fe30dfbcae20a5
wangsunny0403 <wangsunny0403@gmail.com>
2018-10-15 11:36:13 +0800

    修改 1.表格th非粗體 2.選單多展開收合按鈕

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -5,28 +5,27 @@
       <span class="spinner"></span>
     </div>
     <!-- end #page-loader -->
-
+  
     <!-- begin #page-container -->
-    <div id="page-container" class="page-container fade page-sidebar-fixed page-header-fixed">
+    <div id="page-container" class="page-container fade page-sidebar-fixed show page-header-fixed" :class="{'page-sidebar-minified': isHideMenu}">
       <!-- begin #header -->
       <div id="header" class="header navbar-default">
         <!-- begin navbar-header -->
         <div class="navbar-header">
-          <router-link
-              class="navbar-brand" :to="{
-							name: 'welcome'
-						}">
+          <router-link class="navbar-brand" :to="{
+  							name: 'welcome'
+  						}">
             <span class="navbar-logo"></span>
             <b>APP</b> CMS
           </router-link>
           <button type="button" class="navbar-toggle" data-click="sidebar-toggled">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
         </div>
         <!-- end navbar-header -->
-
+  
         <!-- begin header-nav -->
         <ul class="navbar-nav navbar-right p-0">
           <li class="dropdown navbar-user">
@@ -36,29 +35,29 @@
               <b class="caret"></b>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
-
+  
               <router-link class="dropdown-item" :to="{
-                  name: 'edit-profile'
-              }">个人设定
+                    name: 'edit-profile'
+                }">个人设定
               </router-link>
-
+  
               <router-link class="dropdown-item" :to="{
-                  name: 'person-token-generate'
-                }">
+                    name: 'person-token-generate'
+                  }">
                 产生令牌
               </router-link>
-
+  
               <div class="dropdown-divider"></div>
               <a href="javascript:;" class="dropdown-item" @click="logout()">
-                登出
-              </a>
+                  登出
+                </a>
             </div>
           </li>
         </ul>
         <!-- end header navigation right -->
       </div>
       <!-- end #header -->
-
+  
       <!-- begin #sidebar -->
       <div id="sidebar" class="sidebar">
         <!-- begin sidebar scrollbar -->
@@ -79,16 +78,22 @@
           </ul>
           <!-- end sidebar user -->
           <!-- begin sidebar nav -->
-
+  
           <ul class="nav">
             <j-menu v-for="(node, index) in menus" :key="index" :node="node" />
             <li>
-              <a href="javascript:;window.open('https://hackmd.io/s/S1BT9O4q7', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500')"
-                 target="">
+              <a href="javascript:;window.open('https://hackmd.io/s/S1BT9O4q7', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500')" target="">
                 <i class="fas fa-external-link-square-alt" />
                 <span>串接说明</span>
               </a>
             </li>
+            <!-- begin sidebar minify button -->
+            <li>
+              <a href="javascript:;" class="sidebar-minify-btn" @click="isHideMenu = !isHideMenu">
+                <i class="fa fa-angle-double-left"></i>
+              </a>
+            </li>
+            <!-- end sidebar minify button -->
           </ul>
           <!-- end sidebar nav -->
         </div>
@@ -96,11 +101,11 @@
       </div>
       <div class="sidebar-bg"></div>
       <!-- end #sidebar -->
-
+  
       <!-- begin #content -->
       <router-view></router-view>
       <!-- end #content -->
-
+  
       <!-- end scroll to top btn -->
     </div>
   </div>
@@ -109,19 +114,25 @@
 <script>
   import CheckLoginMixins from 'mixins/common/CheckLogin'
   import IndexMixins from 'mixins/common/Index'
-  import { LoginType } from 'module/login'
-
+  import {
+    LoginType
+  } from 'module/login'
+  
   export default {
     metaInfo: {
       title: 'APP后台',
-      link: [
-        {rel: 'stylesheet', href: '/resource/plugins/ionicons/css/ionicons.min.css'}
-      ]
+      link: [{
+        rel: 'stylesheet',
+        href: '/resource/plugins/ionicons/css/ionicons.min.css'
+      }]
     },
     mixins: [CheckLoginMixins, IndexMixins],
     components: {
       JMenu: require('@/shared/Menu1').default
     },
+    data: () => ({
+      isHideMenu: false
+    }),
     methods: {
       logout() {
         this.$store.commit(LoginType.clearAccessToken)
@@ -130,10 +141,8 @@
         })
       },
       dataInit() {
-        axios.all([this.getMyNodes(), this.getAccount()]).then(() =>
-        {
-          this.$nextTick(() =>
-          {
+        axios.all([this.getMyNodes(), this.getAccount()]).then(() => {
+          this.$nextTick(() => {
             App.init()
           })
         })
@@ -154,6 +163,7 @@
           > th
             border-top: 1px solid #b6c2c9 !important
             padding: 8px 10px
+            font-weight: normal
       > tbody
         > tr
           > td
